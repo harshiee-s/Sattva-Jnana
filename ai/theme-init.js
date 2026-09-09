471ac6bfb17379bdef240b107eca0a0434c2b22c
harshiee-s <harshi7354@gmail.com>
2026-09-09 14:32:03 +0530

    Delete ai/theme-init.js

--- ai/theme-init.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  try{
-    var saved = localStorage.getItem('sattva-theme');
-    var theme = saved || 'light';
-    document.documentElement.setAttribute('data-theme', theme);
-  }catch(e){}
-})();
